f102278f937cc243fc4a3d0a6d1a371d496a2cc6
Narek Ohanyan <ohanyanarek@gmail.com>
2021-03-25 16:38:57 -0500

    first commit

---
+++ src/main.c
@@ -0,0 +1,27 @@
+#include "header.h"
+
+/**
+ * Write final word count to a single file.
+ * The path name should be output/result.txt
+ */
+void writeFinalDSToFiles(void) {
+}
+
+int main(int argc, char *argv[]){
+    
+    //TODO: Argument check
+
+    bookeepingCode();
+    
+    //TODO: Initialize global variables, like shared queue
+    
+    //TODO: create producer and consumer threads
+
+    //TODO: wait for all threads to complete execution
+    
+    //Write the final output
+    writeFinalDSToFiles();
+    
+    return 0; 
+}
+
