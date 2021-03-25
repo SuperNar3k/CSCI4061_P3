77768fd4473798474f98bc396490eb53006723fb
Narek Ohanyan <ohanyanarek@gmail.com>
2021-03-25 17:11:20 -0500

    started main.c

--- src/main.c
+++ src/main.c
@@ -7,9 +7,35 @@
 void writeFinalDSToFiles(void) {
 }
 
+int printFlag, boundedFlag, numConsumers, queueSize;
+char *inputFile;
+
+
 int main(int argc, char *argv[]){
     
-    //TODO: Argument check
+    //Argument check
+    if(argc < 2 || argc > 4) {
+        printf("usage: %s <#Consumers> <inputFile> [option] [#queueSize]\n",argv[0]);
+		exit(EXIT_FAILURE);
+    }
+    //Setup
+    printFlag = 0;
+    boundedFlag = 0;
+    queueSize = 0;
+    inputFile = argv[2];
+    numConsumers = strtol(argv[1], NULL, 10);
+
+    //Option flag setup
+    if(argv[3].equals("-p")) {
+        printFlag = 1;
+    } else if(argv[3].equals("-b")) {
+        boundedFlag = 1;
+        queueSize = strtol(argv[4], NULL, 10);
+    } else if(argv[3].equals("-bp")) {
+        printFlag = 1;
+        boundedFlag = 1;
+        queueSize = strtol(argv[4], NULL, 10);
+    }
 
     bookeepingCode();
     
