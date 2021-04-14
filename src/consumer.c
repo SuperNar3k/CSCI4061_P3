0be7ab68a067f0644009eeb956826532495dc440
roxas007 <roxas007@umn.edu>
2021-04-13 21:53:50 -0500

    pretty pretty

--- src/consumer.c
+++ src/consumer.c
@@ -1,18 +1,20 @@
 #include "consumer.h"
 
-/**
- * parse lines from the queue, and count words by word length
- * returns the line number of the item read from the queue
- */
+
+ // Parse lines from the queue, and count words by word length. Returns the line number of the item read from the queue.
+ 
+
 int parse(int* interDS){
+    // Initialize variables, store next data to be read into a node and also copy it into a string
     char line[chunkSize];
     node_t* node = dequeue(queue);
     strcpy(line, node->line);
-
+    // More initialization of variables
     int lineNum = node->lineNum;
     int wordLen = 0;
     int lineLen = strlen(line);
 
+    // Iterate through each line
     for (int i = 0; i < lineLen; i++){            
         // records the length of a word once whitespace or a newline is found
         if (line[i] == ' ' || line[i] == '\n'){
@@ -23,7 +25,9 @@ int parse(int* interDS){
         }
     }
 
-    interDS[wordLen - 1]++; // add length of the last word on the last line
+    // add length of the last word on the last line
+    interDS[wordLen - 1]++; 
+    // Free data
     free(node->line);
     free(node);
     return lineNum;
@@ -38,6 +42,7 @@ void *consumer(void *arg){
     }
     int interDS[MaxWordLength];
 
+    // Use semaphores and mutex locks to access the data and write to the histogram
     while(1){
         sem_wait(&items);
         pthread_mutex_lock(&lock);
--- src/main.c
+++ src/main.c
@@ -1,17 +1,12 @@
 #include "header.h"
 
-/**
- * Write final word count to a single file.
- * The path name should be output/result.txt
- */
+ // Outputs word count to a file with a path named output/result.txt
 void writeFinalDSToFiles(void) {
     char* filePath = "output/result.txt";
     FILE* fp = fopen(filePath, "w");
     
     for (int i = 0; i < MaxWordLength; i++){
         char line[chunkSize];
-        //fprintf(fp, "%d %d\n", i + 1, test[i]);
-        //fprintf(fp, "%d \n", finalDS[i]);
         fprintf(fp, "%d %d\n", i + 1, finalDS[i]);
     }
     fclose(fp);
@@ -19,24 +14,25 @@ void writeFinalDSToFiles(void) {
 
 int main(int argc, char *argv[]){
     
-    //Argument check
+    // Check if all arguments are present
     if(argc < 3 || argc > 5) {
         printf("usage: %s <#Consumers> <inputFile> [option] [#queueSize]\n",argv[0]);
 		exit(EXIT_FAILURE);
     }
     
-    //Setup
+    // Initialize variables
     printFlag = 0;
     boundedFlag = 0;
     queueSize = 0;
     char *inputFile = argv[2];
     int numConsumers = strtol(argv[1], NULL, 10);
 
+    // clear out the array used to write to end file
     for (int i = 0; i < MaxWordLength; i++){
         finalDS[i] = 0;
     }
 
-    //Option flag setup
+    // Optional flag setups
     if(argc > 3){
         if(strcmp(argv[3], "-p") == 0) {
          printFlag = 1;
@@ -51,10 +47,9 @@ int main(int argc, char *argv[]){
         }
     }
     
-
     bookeepingCode();
     
-    //Initialize global variables
+    // Initialize global variables
     queue = malloc(sizeof(queue_t));
     queue->front = NULL;
     queue->back = NULL;
@@ -68,26 +63,28 @@ int main(int argc, char *argv[]){
     }
     sem_init(&items, 0, 0);
 
-    //Create producer and consumer threads
+    // For each producer, create a producer thread
     pthread_t producerThread;
     pthread_t* consumerThreads = malloc(sizeof(pthread_t) * numConsumers);
 
     pthread_create(&producerThread, NULL, producer, (void*) inputFile);
 
     int *consumerIDs = malloc(numConsumers*sizeof(int));
+    // For each consumer, create a consumer thread
     for (int i = 0; i < numConsumers; i++){
         consumerIDs[i] = i;
         pthread_create(&consumerThreads[i], NULL, consumer, &consumerIDs[i]);
     }
 
-    //Wait for all threads to complete execution
+    // Wait for all threads to complete execution
     pthread_join(producerThread, NULL);
     for (int i = 0; i < numConsumers; ++i){
-        pthread_join(consumerThreads[i], NULL); //wait for all the threads to be finished
+        pthread_join(consumerThreads[i], NULL); // wait for all the threads to be finished
     } 
 
     //Write the final output
     writeFinalDSToFiles();
+    // Free memory
     free(consumerIDs);
     free(consumerThreads);
     free(queue);
